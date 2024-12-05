dd3142ca261b36312964c0741b88fbdc0191a840
Minh Vương <hardingadonis@gmail.com>
2024-12-05 08:00:03 +0700

    update: change icon for app

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
     }
 
     buildTypes {
@@ -24,8 +24,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
